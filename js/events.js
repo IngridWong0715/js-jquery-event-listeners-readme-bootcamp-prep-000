6179e0493d697322ac9419b706a49bc0158a38b5
Ingrid Wong <yw340@georgetown.edu>
2017-12-18 06:33:02 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,20 @@
 //define functions here
 
-$(document).ready(function(){
 
+function getIt(){
+  $("p").on("click", function(){
+    alert("Hey!");
+  })
+}
+
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  })
+}
+$(document).ready(function(){
+ getIt();
+ frameIt();
 // call functions here
 
 });
